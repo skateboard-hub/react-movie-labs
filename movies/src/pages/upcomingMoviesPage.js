e6349132b8fbc7d4111e02a7e26350294b5e378e
Shunyi Xu <3051778755@qq.com>
2022-10-25 14:01:40 +0100

    CI - Basic pipeline.

--- movies/src/pages/upcomingMoviesPage.js
+++ movies/src/pages/upcomingMoviesPage.js
@@ -1,32 +1,36 @@
-import React, { useState, useEffect } from "react";
-import PageTemplate from '../components/templateMovieListPage'
+import React from "react";
 import { getupcomingMovies } from "../api/tmdb-api";
+import PageTemplate from '../components/templateMovieListPage';
+import { useQuery } from 'react-query';
+import Spinner from '../components/spinner';
+import AddToFavoritesIcon from '../components/cardIcons/addToFavorites'
 
-const UpcomingPage = (props) => {
-  
-  const [movies, setMovies] = useState([]);
+const HomePage = (props) => {
+
+  const {  data, error, isLoading, isError }  = useQuery('discover', getupcomingMovies)
+
+  if (isLoading) {
+    return <Spinner />
+  }
+
+  if (isError) {
+    return <h1>{error.message}</h1>
+  }  
+  const movies = data.results;
+
+  // Redundant, but necessary to avoid app crashing.
   const favorites = movies.filter(m => m.favorite)
   localStorage.setItem('favorites', JSON.stringify(favorites))
+  const addToFavorites = (movieId) => true 
 
-  const addToFavorites = (movieId) => {
-    const updatedMovies = movies.map((m) =>
-      m.id === movieId ? { ...m, favorite: true } : m
-    );
-    setMovies(updatedMovies);
-  };
-
-  useEffect(() => {
-    getupcomingMovies().then(movies => {
-      setMovies(movies);
-    });
-  }, []);
-  
   return (
     <PageTemplate
-      title='Upcoming Movies'
+      title="Discover Movies"
       movies={movies}
-      selectFavorite={addToFavorites}
+      action={(movie) => {
+        return <AddToFavoritesIcon movie={movie} />
+      }}
     />
   );
 };
-export default UpcomingPage;
+export default HomePage;
