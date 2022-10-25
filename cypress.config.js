fb02993570dd7358a45196ecfc0a61df3fdb89e3
Shunyi Xu <3051778755@qq.com>
2022-10-25 18:20:20 +0100

    CI - Add test stage to the pipeline.

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,21 @@
+//const { defineConfig } = require("cypress");
+
+//module.exports = defineConfig({
+//  e2e: {
+//    setupNodeEvents(on, config) {
+//      // implement node event listeners here
+//    },
+//  },
+//});
+
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  video: false,
   e2e: {
+    "baseUrl": "http://localhost:3000/",
+    "viewportWidth": 1980,
+    "viewportHeight": 1080,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
