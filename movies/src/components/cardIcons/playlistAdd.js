c4f042ae94d7b8941887e40aea4aa38f2aa426bc
Shunyi Xu <3051778755@qq.com>
2022-11-01 13:02:13 +0000

    Tagging upcoming movies as must-watch.

--- movies/src/components/cardIcons/playlistAdd.js
+++ movies/src/components/cardIcons/playlistAdd.js
@@ -4,7 +4,12 @@ import IconButton from "@mui/material/IconButton";
 import PlaylistAddIcon from "@mui/icons-material/PlaylistAdd";
 
 const PlaylistAdd = ({ movie }) => {
-  
+  const context = useContext(MoviesContext);
+
+  const handleAddPlaylist = (e) => {
+    e.preventDefault();
+    context.addPlaylist(movie);
+  };
   return (
     <IconButton aria-label="add to playlist" onClick={handleAddPlaylist} >
       <PlaylistAddIcon color="primary" fontSize="large" />
